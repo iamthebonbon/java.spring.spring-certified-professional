ea2e636e3c038f1f8fdcf3dd2662b64dba7d015b
bonbon <n.u.abdysamat@gmail.com>
2026-06-18 21:59:55 +0600

    first commit

--- src/test/java/com/example/scp/ApplicationTests.java
+++ src/test/java/com/example/scp/ApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.net.URI;
 

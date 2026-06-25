95b25fb941a2c2e9ff82d1be1fe410b83ae14243
bonbon <n.u.abdysamat@gmail.com>
2026-06-25 16:25:25 +0600

    Tests

--- src/test/java/com/example/scp/DbTests.java
+++ src/test/java/com/example/scp/DbTests.java
@@ -1,10 +1,6 @@
 package com.example.scp;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,31 +12,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 class DbTests {
     private static final Logger LOGGER = LoggerFactory.getLogger(DbTests.class);
 
-    @BeforeAll
-    public static void beforeAll() {
-        LOGGER.info("JUnit5@BeforeAll is called");
-    }
-
-    @AfterAll
-    public static void afterAll() {
-        LOGGER.info("JUnit5@AfterAll is called");
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        LOGGER.info("JUnit5@BeforeEach is called");
-    }
-
-    @AfterEach
-    public void afterEach() {
-        LOGGER.info("JUnit5@AfterEach is called");
-    }
-
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     @Test
     void testOne() {
+        LOGGER.info("DbTests is called");
         Assertions.assertEquals(
                 21L,
                 jdbcTemplate.queryForObject("SELECT count(*) FROM T_ACCOUNT", Long.class)

c63d31e8b16c7a2cefaa6fc111ba2875aafa3af6
bonbon <n.u.abdysamat@gmail.com>
2026-06-28 15:51:17 +0600

    Clean up

--- src/test/java/com/example/scp/repository/BonbonRepository.java
+++ src/test/java/com/example/scp/repository/BonbonRepository.java
@@ -2,7 +2,9 @@ package com.example.scp.repository;
 
 import com.example.scp.entity.BonBon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,5 +15,32 @@ public interface BonbonRepository extends JpaRepository<BonBon, Long> {
 
     @Query(value = "select * from bon_bon o where o.candy_type = :type", nativeQuery = true)
     BonBon getByTypeNativeSql(String type);
+
+    /**
+     * doesn't work. there is no @Modifying
+     */
+    @Query(value = "insert into bon_bon(id, candy_type) values (:id, :type)", nativeQuery = true)
+    int createByNativeSqlWithoutModifying(Long id, String type);
+
+    @Modifying
+    @Query(value = "insert into bon_bon(id, candy_type) values (:id, :type)", nativeQuery = true)
+    int createByNativeSql(Long id, String type);
+
+    /**
+     * doesn't work. there is no @Modifying
+     */
+    @Query("UPDATE BonBon o SET o.candyType = :type WHERE o.id = :id")
+    void updateByJpqlWithoutModifying(Long id, String type);
+
+    /**
+     * Cache problem due ORM
+     */
+    @Modifying
+    @Query("UPDATE BonBon o SET o.candyType = :type WHERE o.id = :id")
+    int updateByJpqlWithoutClearCache(@Param("id") Long id, @Param("type") String type);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE BonBon o SET o.candyType = :type WHERE o.id = :id")
+    int updateByJpql(@Param("id") Long id, @Param("type") String type);
 
 }
--- src/test/java/com/example/scp/test/fullcontext/RepositoryE2eTest.java
+++ src/test/java/com/example/scp/test/fullcontext/RepositoryE2eTest.java
@@ -5,7 +5,9 @@ import com.example.scp.repository.BonbonRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +28,18 @@ class RepositoryE2eTest extends AbstractE2eConfiguration {
     // method-level @Sql overrides class-level one
     @Sql(statements = {
             "create table bon_bon (id integer, candy_type varchar(255));",
+            "insert into bon_bon (id, candy_type) values (1, 'chocolate')"
+    })
+    void jdbcTemplateTest() {
+        Integer count = jdbcTemplate.queryForObject("select count(*) from bon_bon", Integer.class);
+        Assertions.assertEquals(1, count);
+    }
+
+    @Test
+    @Transactional
+    // method-level @Sql overrides class-level one
+    @Sql(statements = {
+            "create table bon_bon (id integer, candy_type varchar(255));",
             "insert into bon_bon (id, candy_type) values (1, 'cookie')"
     })
     void jpaTest() {
@@ -40,19 +54,7 @@ class RepositoryE2eTest extends AbstractE2eConfiguration {
             "create table bon_bon (id integer, candy_type varchar(255));",
             "insert into bon_bon (id, candy_type) values (1, 'chocolate')"
     })
-    void jdbcTemplateTest() {
-        Integer count = jdbcTemplate.queryForObject("select count(*) from bon_bon", Integer.class);
-        Assertions.assertEquals(1, count);
-    }
-
-    @Test
-    @Transactional
-    // method-level @Sql overrides class-level one
-    @Sql(statements = {
-            "create table bon_bon (id integer, candy_type varchar(255));",
-            "insert into bon_bon (id, candy_type) values (1, 'chocolate')"
-    })
-    void jpqlTest() {
+    void getByJpqlTest() {
         Integer count = jdbcTemplate.queryForObject("select count(*) from bon_bon", Integer.class);
         Assertions.assertEquals(1, count);
         BonBon chocolate = bonbonRepository.getByTypeJavaPersistenceQueryLangauge("chocolate");
@@ -66,11 +68,98 @@ class RepositoryE2eTest extends AbstractE2eConfiguration {
             "create table bon_bon (id integer, candy_type varchar(255));",
             "insert into bon_bon (id, candy_type) values (1, 'marmalade')"
     })
-    void nativeSqlTest() {
+    void getByNativeSqlTest() {
         Integer count = jdbcTemplate.queryForObject("select count(*) from bon_bon", Integer.class);
         Assertions.assertEquals(1, count);
         BonBon chocolate = bonbonRepository.getByTypeNativeSql("marmalade");
         Assertions.assertEquals("marmalade", chocolate.getCandyType());
+    }
+
+    @Test
+    @Transactional
+    // method-level @Sql overrides class-level one
+    @Sql(statements = {
+            "create table bon_bon (id integer, candy_type varchar(255));",
+            "insert into bon_bon (id, candy_type) values (1, 'cookie')"
+    })
+    void createByNativeSqlWithoutModifyingTest() {
+        JpaSystemException exception = Assertions.assertThrows(JpaSystemException.class, () -> {
+            bonbonRepository.createByNativeSqlWithoutModifying(2L, "marshmallow");
+        });
+        Assertions.assertEquals(
+                "JDBC exception executing SQL [insert into bon_bon(id, candy_type) values (?, ?)] [No results were returned by the query.] [n/a]",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    @Transactional
+    // method-level @Sql overrides class-level one
+    @Sql(statements = {
+            "create table bon_bon (id integer, candy_type varchar(255));",
+            "insert into bon_bon (id, candy_type) values (1, 'cookie')"
+    })
+    void createByNativeSqlTest() {
+        Assertions.assertEquals(1, bonbonRepository.createByNativeSql(2L, "marshmallow"));
+        Assertions.assertNotNull(
+                bonbonRepository.getByTypeNativeSql("marshmallow")
+        );
+    }
+
+    @Test
+    @Transactional
+    // method-level @Sql overrides class-level one
+    @Sql(statements = {
+            "create table bon_bon (id integer, candy_type varchar(255));",
+            "insert into bon_bon (id, candy_type) values (1, 'cookie')"
+    })
+    void updateByJpqlStatusWithoutModifyingTest() {
+        InvalidDataAccessApiUsageException exception = Assertions.assertThrows(InvalidDataAccessApiUsageException.class, () -> {
+            bonbonRepository.updateByJpqlWithoutModifying(1L, "marshmallow");
+        });
+        Assertions.assertEquals(
+                "Query executed via 'getResultList()' or 'getSingleResult()' must be a 'select' query [UPDATE BonBon o SET o.candyType = :type WHERE o.id = :id]",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    @Transactional
+    // method-level @Sql overrides class-level one
+    @Sql(statements = {
+            "create table bon_bon (id integer, candy_type varchar(255));",
+            "insert into bon_bon (id, candy_type) values (1, 'cookie')"
+    })
+    void updateByJpqlWithoutReturnTypeTest() {
+        // Attention!!! Cache still returns same value
+        Assertions.assertEquals(
+                "cookie",
+                bonbonRepository.getReferenceById(1L).getCandyType()
+        );
+        bonbonRepository.updateByJpqlWithoutClearCache(1L, "marshmallow");
+        Assertions.assertEquals(
+                "cookie",
+                bonbonRepository.getByTypeJavaPersistenceQueryLangauge("marshmallow").getCandyType()
+        );
+    }
+
+    @Test
+    @Transactional
+    // method-level @Sql overrides class-level one
+    @Sql(statements = {
+            "create table bon_bon (id integer, candy_type varchar(255));",
+            "insert into bon_bon (id, candy_type) values (1, 'cookie')"
+    })
+    void updateByJpqlTest() {
+        Assertions.assertEquals(
+                "cookie",
+                bonbonRepository.getReferenceById(1L).getCandyType()
+        );
+        bonbonRepository.updateByJpql(1L, "marshmallow");
+        Assertions.assertEquals(
+                "marshmallow",
+                bonbonRepository.getByTypeNativeSql("marshmallow").getCandyType()
+        );
     }
 
 }

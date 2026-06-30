9949b92b5cd4eca24f2f912a2b7ee7af571c6ece
bonbon <n.u.abdysamat@gmail.com>
2026-06-30 16:48:54 +0600

    Application.java

--- src/main/java/com/example/scp/Application.java
+++ src/main/java/com/example/scp/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
 //        app.setWebApplicationType(WebApplicationType.NONE);
+//        app.setBanner();
         app.run(args);
     }
 

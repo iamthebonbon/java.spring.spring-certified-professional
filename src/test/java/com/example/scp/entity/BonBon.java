147323148f04b78e757b670f52dd4881c0a22c95
bonbon <n.u.abdysamat@gmail.com>
2026-07-01 14:20:34 +0600

    @EnableJpaAuditing(auditorAwareRef = "auditorProvider")

--- src/test/java/com/example/scp/entity/BonBon.java
+++ src/test/java/com/example/scp/entity/BonBon.java
@@ -1,14 +1,38 @@
 package com.example.scp.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class BonBon {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,6 +52,38 @@ public class BonBon {
 
     public void setCandyType(String candyType) {
         this.candyType = candyType;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
     }
 
     @Override
--- src/test/java/com/example/scp/test/slice/BonbonRepositorySliceTest.java
+++ src/test/java/com/example/scp/test/slice/BonbonRepositorySliceTest.java
@@ -6,13 +6,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.StreamSupport;
 
 @DataJpaTest
@@ -20,6 +27,19 @@ import java.util.stream.StreamSupport;
         "create table bon_bon (id integer, candy_type varchar(255));"
 })
 class BonbonRepositorySliceTest {
+
+    @TestConfiguration
+    @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+    public static class DataConfiguration {
+
+        @Bean
+        public AuditorAware<String> auditorProvider() {
+            return () -> Optional.ofNullable(
+                    SecurityContextHolder.getContext().getAuthentication()
+            ).map(Authentication::getName);
+        }
+
+    }
 
     @Container
     @ServiceConnection
@@ -33,7 +53,7 @@ class BonbonRepositorySliceTest {
     @Test
     // method-level @Sql overrides class-level one
     @Sql(statements = {
-            "create table bon_bon (id integer, candy_type varchar(255));",
+            "create table bon_bon (id integer, created_at timestamp, updated_at timestamp, created_by varchar(255), updated_by varchar(255), candy_type varchar(255));",
             "insert into bon_bon (id, candy_type) values (1, 'cookie')"
     })
     void test() {
@@ -41,6 +61,20 @@ class BonbonRepositorySliceTest {
         Assertions.assertEquals(1, all.size());
         Integer count = jdbcTemplate.queryForObject("select count(*) from bon_bon", Integer.class);
         Assertions.assertEquals(1, count);
+    }
+
+    @Test
+    // method-level @Sql overrides class-level one
+    @Sql(statements = {
+            "create table bon_bon (id bigserial primary key, created_at timestamp, updated_at timestamp, created_by varchar(255), updated_by varchar(255), candy_type varchar(255));",
+            "insert into bon_bon (candy_type) values ('cookie')"
+    })
+    void testSave() {
+        BonBon bonBon = new BonBon();
+        bonBon.setCandyType("bubblegum");
+        bonbonRepository.save(bonBon);
+        Assertions.assertNotNull(bonBon.getCreatedAt());
+        Assertions.assertNotNull(bonBon.getUpdatedAt());
     }
 
 }
